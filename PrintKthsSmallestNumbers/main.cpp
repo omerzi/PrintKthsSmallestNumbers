dffc5f1d6d38de49bf647076a80243fe252c351a
omerzi <50792403+omerzi@users.noreply.github.com>
2020-05-15 08:04:51 +0300

    an Example

--- PrintKthsSmallestNumbers/main.cpp
+++ PrintKthsSmallestNumbers/main.cpp
@@ -2,8 +2,14 @@ using namespace std;
 #include <iostream>
 #include <stdlib.h>
 
+// ------------------------------------------------------------------------------//
+// A Data Structures course project in C++, 
+// printing all the values that are smaller than a given key in 3 different ways:
+// Naive (using sorted Linked List), Binary Search Tree, and by using Quicksort.
+// Made by : Omer Zidkoni 316498294 and Naor Pashay
+// -----------------------------------------------------------------------------//
 
 int main()
 {
-
+	cout << "Hello";
 }
